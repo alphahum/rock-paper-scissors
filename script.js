033b8a2bd9a7748ca70a817558d69e2f6f42ef42
alphahum <216708197+alphahum@users.noreply.github.com>
2025-07-29 18:49:26 +0500

    Add game logic and scoring for Rock Paper Scissors in script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanscore = 0;
+let computerscore = 0;
+
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
 
@@ -11,3 +14,31 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let input = prompt("Choose rock, paper, or scissors:");
+    return input;
+}
+
+function playRound( humanChoice, computerChoice ) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both choose ${humanChoice}.`);
+    }
+    else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+                console.log (`You win this round! ${humanChoice} beats ${computerChoice}.`);
+                humanscore++;
+            }
+            else {
+                console.log (`You lose this round! ${computerChoice} beats ${humanChoice}.`);
+                computerscore++;
+            }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
